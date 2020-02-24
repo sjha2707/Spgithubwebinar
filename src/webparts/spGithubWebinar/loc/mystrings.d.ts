ccce97a444117c521ef6bb666c7b5352c57ebe77
Sandeep Jha <sjha2707@gmail.com>
2020-02-24 16:13:05 -0500

    intial comitt fixes AB#1

---
+++ src/webparts/spGithubWebinar/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpGithubWebinarWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpGithubWebinarWebPartStrings' {
+  const strings: ISpGithubWebinarWebPartStrings;
+  export = strings;
+}
